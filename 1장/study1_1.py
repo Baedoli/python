2cfb24dd2666b09aa665dcf669b498eb6760d227
Bae Sungho <bae9808.gmail.com>
2018-04-29 15:17:16 +0900

    second vs code

---
+++ 1장/study1_1.py
@@ -0,0 +1,36 @@
+
+movies = ["The1", "The2", "The3"]
+
+movies1 = ["The Holy Grail",1975, "Terry Jones & Terry Gilliam", 91,
+           ["Graham Chapma", ["Michael Palin", "John Cleese", "Terryu Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print(len(movies))
+print(movies[1])
+
+for list_movie in movies:
+    print(list_movie)
+
+count_ = 0
+while count_ < len(movies):
+    print(movies[count_])
+    count_ = count_ + 1
+
+print(isinstance(movies,list))
+
+leng_movies = len(movies)
+
+print(isinstance(leng_movies,list))
+
+for each_item in movies1:
+    if isinstance(each_item,list):
+        for each_item1 in each_item:
+            if isinstance(each_item1,list):
+                for each_item2 in each_item1:
+                    print(each_item2)
+            else:
+                print(each_item1)
+    else:
+        print(each_item)
+
+
+
